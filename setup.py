df62a3d2d6db1e50fdd1fce30c5986929a1c6605
lucaslealll <lucasoalzst@gmail.com>
2024-10-20 20:02:55 +0100

    att

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='InstagramFollowersAndUnfollowers',
-    version='1.0.0',
+    name="InstagramFollowersAndUnfollowers",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        'requests',
-        'setuptools',
+        "requests",
+        "setuptools",
+        "selenium",
+        "undetected-chromedriver",
     ],
-    entry_points={
-        'console_scripts': [
-            'fetch-results=scripts.fetch:main',
-        ],
-    },
 )
