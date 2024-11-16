186648b513cb87b3a18b80e1936e2e019d905613
lucaslealll <lucasoalzst@gmail.com>
2024-11-16 19:04:19 +0000

    add pack de inicializacao do diretorio como importavel

---
+++ scripts/components/__init__.py
@@ -0,0 +1,22 @@
+from components.followers import *
+from components.following import *
+from components.instagram import *
+from components.profile import *
+from components.utils import *
+
+__all__ = [
+    "getUserFollowers",
+    "getUserFollowing",
+    "instagram_request",
+    "getUserProfileInfo",
+    "XIGAPPID",
+    "BASE_URL_USERS",
+    "BASE_URL_FRIENDSHIPS",
+    "bold,",
+    "italic,",
+    "underline,",
+    "strikethrough",
+    "reset_color",
+    "green",
+    "red",
+]
