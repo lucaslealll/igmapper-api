370f8d335c03b9129855c5120d9354b040963283
lucaslealll <lucas.o.a.l.zst@gmail.com>
2024-11-30 13:08:12 +0000

    ordenando funcoes

--- igmapper/components/utils.py
+++ igmapper/components/utils.py
@@ -2,25 +2,25 @@ def bold(text):
     return f"\033[1m{text}\033[0m"
 
 
+def green(text):
+    return f"\033[32m{text}\033[0m"
+
+
 def italic(text):
     return f"\033[3m{text}\033[0m"
 
 
-def underline(text):
-    return f"\033[4m{text}\033[0m"
-
-
-def strikethrough(text):
-    return f"\033[9m{text}\033[0m"
+def red(text):
+    return f"\033[31m{text}\033[0m"
 
 
 def reset_color(text):
     return f"\033[0m{text}"
 
 
-def green(text):
-    return f"\033[32m{text}\033[0m"
+def strikethrough(text):
+    return f"\033[9m{text}\033[0m"
 
 
-def red(text):
-    return f"\033[31m{text}\033[0m"
+def underline(text):
+    return f"\033[4m{text}\033[0m"
